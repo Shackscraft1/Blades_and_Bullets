425e4adba204f0d518b91fc51e5582d2ad49257c
Shackscraft <joelozano@csumb.edu>
2026-04-16 05:47:24 -0700

    changes showed during milesstone 1

---
+++ Blades_and_Bullets/Assets/Scripts/Joel_Scripts/AbilityBarProgress.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityBarProgress : MonoBehaviour
+{
+    [SerializeField] private Image barImage;
+    [SerializeField] private float duration = 0f;
+    public event EventHandler Ondeath;
+
+    private class OndeathArgs : EventArgs
+    {
+        private int hp;
+        private int maxHp;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        barImage.type = Image.Type.Filled;
+        barImage.fillMethod = Image.FillMethod.Vertical;
+        barImage.fillAmount = 0f;
+        StartCoroutine(FillAbilityProgressBar());
+        Ondeath.Invoke?(this,  EventArgs.Empty);
+
+    }
+
+    IEnumerator FillAbilityProgressBar()
+    {
+        
+        while (duration <= 1f)
+        {
+            Debug.Log("test");
+            yield return new WaitForSeconds(1f);
+            duration += .1f;
+            barImage.fillAmount = duration;
+           
+        }
+        
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
