ea380b7cadaf63bbddb8699dc9b0aa92d305ad95
Ethan <ethan.g.huang@gmail.com>
2026-04-28 02:49:07 -0700

    Remake EnemyWaves

---
+++ Blades_and_Bullets/Assets/Scripts/Scripts-Ethan.H/SOScripts-Ethan.H/WaveSO.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaveSO", menuName = "WaveData/WaveSO")]
+public class WaveSO : ScriptableObject
+{
+    [Header("Enemy")]
+    public GameObject enemyPrefab;
+    public int enemyCount = 6;
+
+    [Header("Entry")]
+    public float entryDuration = 6f;
+    public float entryGap = 0.35f;
+
+    [Header("Exit")]
+    public WaveEnemy.EndBehavior endBehavior;
+
+    [Header("Formation Shape")]
+    public float slotSpacing = 0.3f;
+    public float vDepth = 0.18f;
+
+    [Header("Formation Movement")]
+    
+    public Vector3 middleOffset = Vector3.zero;
+    public Vector3 leftOffset = new Vector3(-2f, 0f, 0f);
+    public Vector3 rightOffset = new Vector3(2f, 0f, 0f);
+    public float formationTravelSpeed = 2f;
+    public float waitAtLeft = 0.5f;
+    public float waitAtRight = 0.5f;
+    public float waitAtMiddle = 0.5f;
+}
+
+
+
+
+
